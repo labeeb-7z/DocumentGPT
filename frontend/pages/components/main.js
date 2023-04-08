167aec46ff029fce00a92dfe3a7b09a7a01bc342
Smit1603 <smuffinboy16@gmail.com>
2023-04-08 15:14:40 +0530

    Summary

--- frontend/pages/components/main.js
+++ frontend/pages/components/main.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import Qna from './qna'
 import PdfViewer from './pdf-viewer'
+import Summary from './summary'
 
 const Main = () => {
   return (
-    <div className='bg-[#121416]'>
-      <div className='w-[50%] pt-20 pl-20 pr-10'>
-        <div className='border p-5 rounded'>
+    <div className="bg-[#121416] flex">
+      <div className="w-[50%]  pt-20 pl-20 pr-10">
+        <div className="border p-5 rounded">
           <PdfViewer />
         </div>
       </div>
+      <div className="w-[50%] pt-20 pr-20 pl-10">
+        <div className="border p-5 rounded">
+          <Summary />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Main
