8e974b854e5feb0f8b05c43da837c255857db062
Sameer Gupta <sameergupta4873@gmail.com>
2023-04-08 13:20:30 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        '3xl': '0px 0px 300px 10px rgba(255, 255, 251, 0.2)',
+      }
+    },
+  },
+  plugins: [],
+}
+
