25d980128a4c41e5aff8e14a2d85839551f7d4ff
Sameer Gupta <sameergupta4873@gmail.com>
2023-04-08 15:19:50 +0530

    main.js

--- frontend/pages/components/main.js
+++ frontend/pages/components/main.js
@@ -5,7 +5,7 @@ import PdfViewer from './pdf-viewer'
 const Main = () => {
   return (
     <div className='bg-[#121416]'>
-      <div className='w-[50%] pt-20 pl-20 pr-10'>
+      <div className='w-[50%] pt-20 pl-16 pr-10'>
         <div className='border p-5 rounded'>
           <PdfViewer />
         </div>
