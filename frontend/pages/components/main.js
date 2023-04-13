fc8b8e0984f24795ea52d380a454c8b30a7ea012
Sameer Gupta <sameergupta4873@gmail.com>
2023-04-13 23:59:38 +0530

    new

--- frontend/pages/components/main.js
+++ frontend/pages/components/main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Qna from './qna'
 import PdfViewer from './pdf-viewer'
 import Summary from './summary'
