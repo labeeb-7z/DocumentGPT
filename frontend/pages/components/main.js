d7719b270031eb8c927b12e42cd9f9a2e87be013
Sameer Gupta <sameergupta4873@gmail.com>
2023-04-13 23:48:01 +0530

    do

--- frontend/pages/components/main.js
+++ frontend/pages/components/main.js
@@ -673,7 +673,6 @@ const Main = () => {
                   type="button"
                   class="text-white mx-4 bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-7 py-2.5 text-center mr-2 mb-2"
                 >
-                  {" "}
                   Ask
                 </button>
               </div>
